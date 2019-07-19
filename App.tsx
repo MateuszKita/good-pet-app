b3197a06e0d869f3ff483af31eba1f0d39b57497
Mateusz Kita <mkita.zg@gmail.com>
2019-07-19 20:54:05 +0200

    Import native-base and use example button

--- App.tsx
+++ App.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
-import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import React, {Component} from 'react'
+import {ActivityIndicator} from 'react-native'
+import {StyleSheet, View, Image, Dimensions, Text} from 'react-native';
+import {Button} from 'native-base';
+import {Font} from 'expo'
+import {Ionicons} from "@expo/vector-icons";
 
-export default function App() {
-    let mainLogoSourcePath = "./assets/main-logo.png";
+export default class App extends Component {
+    state = {
+        isReady: false
+    };
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.titleText}>GoodPet</Text>
-            <Text>Hello world!</Text>
-            <Text>{"\n"}</Text>
-            <Image source={require(mainLogoSourcePath)} style={styles.mainLogo}/>
-        </View>
-    );
+    componentWillMount = async () => {
+        await Font.loadAsync({
+            Roboto: require('native-base/Fonts/Roboto.ttf'),
+            Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+            ...Ionicons.font
+        });
+        setTimeout(() => {
+            this.setState({isReady: true})
+        }, 1000)
+    };
+
+    render() {
+        const mainLogoSourcePath = "./assets/main-logo.png";
+        if (!this.state.isReady) {
+            return (
+                <View style={styles.container}>
+                    <ActivityIndicator/>
+                </View>
+            )
+        }
+        return (
+            <View style={styles.container}>
+                <Text style={styles.titleText}>GoodPet</Text>
+                <Text>Hello world!</Text>
+                <Text>{"\n"}</Text>
+                <Image source={require(mainLogoSourcePath)} style={styles.mainLogo}/>
+
+                <Button rounded>
+                    <Text>Primary</Text>
+                </Button>
+            </View>
+        );
+    }
 }
 
 const dimensions = Dimensions.get('window');
